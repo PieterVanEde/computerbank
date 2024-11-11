2ad217f34e5b5c4473c193549dc1678a85442824
Pieter van Ede <pietervanede@hotmail.com>
2024-11-11 19:55:18 +0100

    Order desktop icons

--- .config/computerbank/arrange-desktop-icons.sh
+++ .config/computerbank/arrange-desktop-icons.sh
@@ -3,26 +3,18 @@
 # USAGE:
 # adduser.local USER UID GID HOME
 
-#if [ -f ~/.config/computerbank/reboot ]
-#then
-#
-#
-#
-#	xfce4-session-logout --reboot
-#	rm -f ~/.config/computerbank/reboot
-#	exit 0
-#fi
-#
-#if [ -f ~/.config/computerbank/firstlogin ]
-#then
-#	iconsFile=$(readlink ~/.config/xfce4/desktop/icons.screen.latest.rc)
-#	rm $iconsFile
-#	cat ~/.config/computerbank/icons.screen.rc > $iconsFile
-#	perl -i -0pe "s@\~@$HOME@mg" $iconsFile
-#	
-#	chattr +i ~/.config/xfce4/desktop/icons.screen*
-#	
-#	rm -f ~/.config/computerbank/firstlogin
-#fi
+if [ -f ~/.config/computerbank/firstlogin ]
+then
+	iconsFile=$(readlink ~/.config/xfce4/desktop/icons.screen.latest.rc)
+	rm $iconsFile
+	cat ~/.config/computerbank/icons.screen.rc > $iconsFile
+	perl -i -0pe "s@\~@$HOME@mg" $iconsFile
+	
+	xfdesktop --quit
+	sleep 10
+	xfdesktop
+	
+	rm -f ~/.config/computerbank/firstlogin
+fi
 
 
