cab272e30d455e82d57a6fe22cf9256dabf122bc
Pieter van Ede <pietervanede@hotmail.com>
2024-11-11 21:11:30 +0100

    Restart desktop after icons file is created

--- .config/computerbank/arrange-desktop-icons.sh
+++ .config/computerbank/arrange-desktop-icons.sh
@@ -3,18 +3,22 @@
 # USAGE:
 # adduser.local USER UID GID HOME
 
-if [ -f ~/.config/computerbank/firstlogin ]
-then
+iconsFile=$(readlink ~/.config/xfce4/desktop/icons.screen.latest.rc)
+
+while [ -z "${iconsFile}" ]
+do
+	sleep 5
 	iconsFile=$(readlink ~/.config/xfce4/desktop/icons.screen.latest.rc)
-	rm $iconsFile
-	cat ~/.config/computerbank/icons.screen.rc > $iconsFile
-	perl -i -0pe "s@\~@$HOME@mg" $iconsFile
-	
-	xfdesktop --quit
-	sleep 10
-	xfdesktop
-	
-	rm -f ~/.config/computerbank/firstlogin
-fi
+done
+
+rm $iconsFile
+cat ~/.config/computerbank/icons.screen.rc > $iconsFile
+perl -i -0pe "s@\~@$HOME@mg" $iconsFile
+
+xfdesktop --quit
+sleep 10
+xfdesktop
+
+rm -f ~/.config/autostart/arrange.desktop
 
 
